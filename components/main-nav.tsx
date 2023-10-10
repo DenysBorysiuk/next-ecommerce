7742f6ca4e798f8d9bc69d2f69aa7007b277101e
Denys Borysiuk <denisborisiuk@gmail.com>
2023-10-10 21:57:09 +0300

    fix products link and add route

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -35,9 +35,9 @@ export function MainNav({ className, ...props }: React.HTMLAttributes<HTMLElemen
       active: pathname === `/${params.storeId}/colors`,
     },
     {
-      href: `/${params.storeId}/Products`,
+      href: `/${params.storeId}/products`,
       label: 'Products',
-      active: pathname === `/${params.storeId}/Products`,
+      active: pathname === `/${params.storeId}/products`,
     },
     {
       href: `/${params.storeId}/settings`,
